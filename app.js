6562037382dc3b346f8fc056767697c8b2b418d3
levinemr2 <levinemr2@vcu.edu>
2019-03-26 18:07:46 -0400

    updated js, added city and artist options in appropriate divs

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
+$("#cityBtn").on("click", function() {
+  event.preventDefault();
+  var show = $(this).attr("data-show");
+  var queryURL =
+    "https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=sXaOY3gZG8DXBK0h5AAJIFPCfflg5Cg2&city=" +
+    $("#cityInput").val();
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    var results = response._embedded.events;
+    console.log(results);
+    for (var i = 0; i < results.length; i++) {
+      var eventDiv = $("<div>");
+      var event = results[i].name;
+      var date = results[i].dates.start.localDate;
+      var p = $("<p>").html(event + "<br>" + date);
+      
+
+      eventDiv.prepend(p);
+      $("#cityDump").prepend(eventDiv);
+    }
+  });
+});
+
+
 $("#artistBtn").on("click", function() {
     event.preventDefault();
-    var show = $(this).attr("data-show");
-    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=sXaOY3gZG8DXBK0h5AAJIFPCfflg5Cg2&city=richmond";
-
+    var artist = $(this).attr("data-artist");
+    
     $.ajax({
-        url: queryURL,
+        url: "https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=sXaOY3gZG8DXBK0h5AAJIFPCfflg5Cg2&keyword=" +
+        $("#artistInput").val(),
         method: "GET"
-    }).then(function(response) {
+    }).then(function(response){
         var results = response._embedded.events;
         console.log(results);
         for (var i = 0; i < results.length; i++) {
-            var eventDiv = $("<div>");
+            var artistDiv = $("<div>");
             var event = results[i].name;
-            var p = $("<p>").text(event);
-
-
-            eventDiv.prepend(p);
-            $("#artistDump").prepend(eventDiv);
-
-        }
-
+            var date = results[i].dates.start.localDate;
+            var p = $("<p>").html(event + "<br>" + date);
+            
+      
+            artistDiv.prepend(p);
+            $("#artistDump").prepend(artistDiv);
+          }
     });
 });
